e41014130408d21955cfd9765ed56b0e382b9612
Bettina Pedro <bettinapedro93@gmail.com>
2025-03-09 22:53:15 +0100

    Added config for support file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,6 +3,7 @@ import './commands';
 
 //Global setup for the specs
 beforeEach(() => {
+    console.log('Before Each Hook is running');
     cy.visit("https://wave-trial.getbynder.com/login/");
     cy.acceptCookies();
 });
