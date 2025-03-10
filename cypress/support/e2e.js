0e74647c2af4402aea92c97a58e0e23f3690ff14
Bettina Pedro <bettinapedro93@gmail.com>
2025-03-10 11:08:50 +0100

    added missing env variables in yml file

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -4,6 +4,6 @@ import './commands';
 //Global setup for the specs
 beforeEach(() => {
     cy.visit("https://wave-trial.getbynder.com/login/");
-    //cy.wait(4000);
-    cy.acceptCookies();
+    cy.wait(4000);
+    //cy.acceptCookies();
 });
