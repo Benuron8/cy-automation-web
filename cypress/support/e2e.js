d868d34b81bb7aaf84f0d2e9f34a5ce7c1977a25
Bettina Pedro <bettinapedro93@gmail.com>
2025-03-10 11:19:01 +0100

    added cookies accept

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,7 +3,7 @@ import './commands';
 
 //Global setup for the specs
 beforeEach(() => {
-    cy.visit("https://wave-trial.getbynder.com/login/");
     cy.wait(4000);
+    cy.visit("https://wave-trial.getbynder.com/login/");
     cy.acceptCookies();
 });
