9ee06eaaa1693cd56614a45e354f0c9cad72c1e4
Bettina Pedro <bettinapedro93@gmail.com>
2025-03-10 12:37:19 +0100

    added expect after visit browser

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,7 +3,10 @@ import './commands';
 
 //Global setup for the specs
 beforeEach(() => {
-    cy.wait(4000);
+    //cy.wait(4000);
     cy.visit("https://wave-trial.getbynder.com/login/");
+    cy.get('.login-btn')
+    .should('be.visible');
+
     cy.acceptCookies();
 });
