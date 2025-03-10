f3929b02097e6fd794074c5f334aad7c0c73186b
Bettina Pedro <bettinapedro93@gmail.com>
2025-03-10 12:45:50 +0100

    added baseURL in config

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -3,8 +3,7 @@ import './commands';
 
 //Global setup for the specs
 beforeEach(() => {
-    //cy.wait(4000);
-    cy.visit("https://wave-trial.getbynder.com/login/");
+    cy.visit('/login');
     cy.get('.login-btn')
     .should('be.visible');
 
