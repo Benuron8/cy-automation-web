08adc44fc28380f4d84289472572f1dd50f7023f
Bettina Pedro <bettinapedro93@gmail.com>
2025-03-12 22:12:43 +0100

    Update for cicd

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,15 +1,14 @@
 Cypress.Commands.add("login", (email, password) => {
-  cy.url().should('include', '/login/')
-  cy.get('#inputEmail').type(email);
-  cy.get('#inputPassword').type(password, { log: false });
+  cy.url().should('include', '/login/', { timeout: 30000 })
+  cy.get('#inputEmail').should('be.visible').type(email);
+  cy.get('#inputPassword').should('be.visible').type(password, { log: false });
   cy.get('.login-btn').click();
 });
 
 Cypress.Commands.add("loginConfirmation", () => {
   //confirm login successful
   cy.log(cy.url())
-  cy.url().should("include", "/account/dashboard/", { timeout: 30000 });
-  cy.get('.admin-dropdown.profile').should('be.visible')
+  cy.get('#rootHeaderNavigation').should('be.visible', { timeout: 30000 });
 });
 
 Cypress.Commands.add("logoutConfirmation", () => {
@@ -20,36 +19,12 @@ Cypress.Commands.add("logoutConfirmation", () => {
 });
 
 Cypress.Commands.add("confirmError", (message) => {
-
+  cy.url().should('include', '/login/', { timeout: 30000 })
   cy.contains('.login-container .cbox_messagebox_error', 'You have entered an incorrect username or password.', { timeout: 30000 })
     .should('be.visible');
 
   cy.contains('.update.error h1', 'You have entered an incorrect username or password.', { timeout: 30000 })
     .should('be.visible');
-
-  //cy.get('body').then(($body) => {
-  //if ($body.find('.login-container .cbox_messagebox_error').length > 0) {
-  // cy.get('.login-container .cbox_messagebox_error', { timeout: 50000 })
-  //   .should('be.visible')
-  //  .should('contain.text', 'You have entered an incorrect username or password.');
-  // } else {
-  // cy.log('Error message not found in .cbox_messagebox_error');
-  // }
-  //});
-
-  //cy.get('body').then(($body) => {
-  //if ($body.find('.update.error').length > 0) {
-  //cy.get('.update.error', { timeout: 50000 })
-  // .should('exist')
-  // .and('be.visible')
-  // .within(() => {
-  //cy.get('h1').should('contain.text', 'You have entered an incorrect username or password.');
-  // });
-  // } else {
-  cy.log('Error message not found in .update.error');
-  //}
-  //});
-
 });
 
 Cypress.on('uncaught:exception', (err, runnable) => {
